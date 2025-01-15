36e0796bb906310dfa8bd7915b6c9f38d7af97e7
Anchors <ynwaspectre@gmail.com>
2025-01-15 10:23:20 +0800

    Update court color names and values in court store: changed '标准紫' to '奥运紫' and updated the hex value for '笨猪粉' to a lighter shade.

--- src/stores/court.js
+++ src/stores/court.js
@@ -7,9 +7,9 @@ export const useCourtStore = defineStore('court', {
         courtColors: [
             { value: '#4CAF50', hex: '#4CAF50', name: '标准绿', key: 'COLOR1' },
             { value: '#F44336', hex: '#F44336', name: '世界羽联年终红', key: 'COLOR2' },
-            { value: '#9C27B0', hex: '#9C27B0', name: '标准紫', key: 'COLOR3' },
+            { value: '#9C27B0', hex: '#9C27B0', name: '奥运紫', key: 'COLOR3' },
             //加一个粉色
-            { value: '#FBDFDF', hex: '#FBDFDF', name: '笨猪粉', key: 'COLOR4' }
+            { value: '#FFB6C1', hex: '#FFB6C1', name: '笨猪粉', key: 'COLOR4' }
 
         ]
     }),
